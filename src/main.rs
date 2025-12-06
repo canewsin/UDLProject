271c942702f1976ce3f4b42e5a9afe90669903ae
pramukesh <canews.in@gmail.com>
2025-12-06 13:05:39 +0530

    cli: extract file/dir names as const values

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,23 @@ use udl::*;
 
 use crate::rust::RustGenerator;
 
+const EXAMPLES_DIR: &str = "examples";
+const OUTPUT_DIR: &str = "gen";
+const OUTPUT_FILE: &str = "output.rs";
+
 fn main() {
-    let file = std::fs::File::open("examples/udl-example.udl").unwrap();
+    let file = std::fs::File::open(format!("{EXAMPLES_DIR}/udl-example.udl")).unwrap();
     let str = std::io::read_to_string(file).unwrap();
     let udl: UDL = serde_yaml::from_str(&str).unwrap();
-    println!("{:#?}", udl);
+    // println!("{:#?}", udl);
     let generator = RustGenerator();
     let code = generator.generate(&udl);
 
-    let output_file = std::fs::File::create("gen/output.rs").unwrap();
+    let _ = std::fs::create_dir_all(OUTPUT_DIR);
+    let output_file = std::fs::File::create(format!("{OUTPUT_DIR}/{OUTPUT_FILE}")).unwrap();
     let mut output = std::io::BufWriter::new(output_file);
     output.write_all(code.as_bytes()).unwrap();
     output.flush().unwrap();
 
-    RustGenerator::format(&generator, "gen/output.rs");
+    RustGenerator::format(&generator, &format!("{OUTPUT_DIR}/{OUTPUT_FILE}"));
 }
